06a4617adc34890f13ee54f4b566a76539e55163
Cecilia Nilsson <cecilia.o.nilsson@live.se>
2021-02-18 11:01:06 +0100

    Added AddSharkData(), cleaner code.

--- SharkFamilyTree/Database.cs
+++ SharkFamilyTree/Database.cs
@@ -75,6 +75,18 @@ namespace SharkFamilyTree
             ExecuteSQLWithPar(sql, parameters);
         }
 
+        //Add sharks to database
+        public void AddSharkData()
+        {
+            AddPerson(new PersonsOrSharks { firstname = "Grandma", lastname = "Shark", birthYear = 1961 });
+            AddPerson(new PersonsOrSharks { firstname = "Grandpa", lastname = "Shark", birthYear = 1959 });
+            AddPerson(new PersonsOrSharks { firstname = "Daddy", lastname = "Shark", birthYear = 1986, parent1Id = 1, parent2Id = 2 });
+            AddPerson(new PersonsOrSharks { firstname = "Mommy", lastname = "Von Sharkton", birthYear = 1989, parent1Id = 6, parent2Id = 5 });
+            AddPerson(new PersonsOrSharks { firstname = "Granpapy", lastname = "Von Sharkton", birthYear = 1954 });
+            AddPerson(new PersonsOrSharks { firstname = "Granny", lastname = "Von Sharkton", birthYear = 1964, deathYear = 2012 });
+            AddPerson(new PersonsOrSharks { firstname = "Aunty", lastname = "Von Sharkton", birthYear = 1985, parent1Id = 6, parent2Id = 5 });
+        }
+
         //SQL method without parameters
         private void ExecuteSQL(string sql)
         {
--- SharkFamilyTree/Program.cs
+++ SharkFamilyTree/Program.cs
@@ -13,14 +13,7 @@ namespace SharkFamilyTree
 
             db.CreateDatabase(databasName);
             db.CreateTables(tableName);
-
-            db.AddPerson(new PersonsOrSharks { firstname = "Grandma", lastname = "Shark", birthYear = 1961 });
-            db.AddPerson(new PersonsOrSharks { firstname = "Grandpa", lastname = "Shark", birthYear = 1959 });
-            db.AddPerson(new PersonsOrSharks { firstname = "Daddy", lastname = "Shark", birthYear = 1986, parent1Id = 1, parent2Id = 2 });
-            db.AddPerson(new PersonsOrSharks { firstname = "Mommy", lastname = "Von Sharkton", birthYear = 1989, parent1Id = 6, parent2Id = 5 });
-            db.AddPerson(new PersonsOrSharks { firstname = "Granpapy", lastname = "Von Sharkton", birthYear = 1954 });
-            db.AddPerson(new PersonsOrSharks { firstname = "Granny", lastname = "Von Sharkton", birthYear = 1964, deathYear = 2012 });
-            db.AddPerson(new PersonsOrSharks { firstname = "Aunty", lastname = "Von Sharkton", birthYear = 1985, parent1Id = 6, parent2Id = 5 });
+            db.AddSharkData();
 
 
 
